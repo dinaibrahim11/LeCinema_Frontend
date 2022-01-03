4a39ee9acb9f66d1c53a46023929e3428cc91d37
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2022-01-03 15:58:08 +0200

    Homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,28 +2,48 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 import MovieList from './MovieList';
+import MovieListHeading from './MovieListHeading';
+import SearchBox from './SearchBox';
 
 const Home = () => {
-	const [movies, setMovies] = useState([
-        {
-          "_id": "61d28d984cfe59c0ce1642a3",
-          "title": "THE HILL",
-          "date": "10/21/2021",
-          "startTime": "06:00",
-          "endTime": "08:30",
-          "screenRoom": 1,
-          "posterImage": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fbc%2Fd5%2Fc9%2Fbcd5c9519581acc60bd60a429ab0c88f.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F255368241358397299%2F&tbnid=ihnlbFcCyl_zKM&vet=12ahUKEwjMobLP1JT1AhVS4BoKHYHlBRgQMygAegUIARDRAQ..i&docid=jwJfzwCWAGfRPM&w=1131&h=1600&itg=1&q=movie%20poster%20images&ved=2ahUKEwjMobLP1JT1AhVS4BoKHYHlBRgQMygAegUIARDRAQ",
-          "__v": 0,
-          "id": "61d28d984cfe59c0ce1642a3"
-        }
-      ]);
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+        //  const url = 'http://localhost:3000/api/movies';
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
 	
 	return (
-        <div className='container-fluid movie-app'>
-        <div className='row'>
-            <MovieList movies={movies} />
-        </div>
-    </div>
+		<div className='container-fluid movie-app'>
+		<div className='row d-flex align-items-center mt-4 mb-4'>
+			<MovieListHeading heading="What's on" />
+			<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+		</div>
+		<div className='row'>
+			<MovieList movies={movies} />
+		</div>
+		<div className='row d-flex align-items-center mt-4 mb-4'>
+			<MovieListHeading heading="Coming soon" />
+			
+		</div>
+		<div className='row'>
+			<MovieList movies={movies} />
+		</div>
+	</div>
+
+
 	);
 };
 
