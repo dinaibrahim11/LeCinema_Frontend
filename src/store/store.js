2c779baea95814ac56f0db42ede4b144c5689c09
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2022-01-03 13:31:49 +0200

    updating project repo

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './user-slice';
-import postsSlice from './posts-slice';
 
 const store = configureStore({
     reducer: {
         user: userSlice.reducer,
-        posts: postsSlice.reducer
+        
     }
 });
 
--- src/storev2/store.js
+++ src/storev2/store.js
@@ -1,7 +1,5 @@
 import { configureStore, createStore } from '@reduxjs/toolkit';
 import usersSlice from './users-slice';
-import postsSlice from './posts-slice';
-import commentsSlice from './comments-slice';
 import { loadState, saveState } from './localStorage';
 import { throttle } from 'lodash';
 
@@ -10,8 +8,6 @@ import { throttle } from 'lodash';
 const store = configureStore({
     reducer: {
         users: usersSlice.reducer,
-        posts: postsSlice.reducer,
-        comments: commentsSlice.reducer
     },
     preloadedState: loadState()
 });
